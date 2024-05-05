01778c67bf3bf5f7b10e8151ff1b116270fe67c2
tmpelay <tmpmalleret@gmail.com>
2024-05-05 19:52:56 -0300

    env

--- src/index.js
+++ src/index.js
@@ -17,6 +17,21 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/search", async (req, res) => {
+  console.log(req.url.split);
+  const searchImages = async (search) => {
+    return fetch(
+      `https://api.unsplash.com/search/photos/?query=${search}&client_id=${process.env.ACCESS_TOKEN}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.results;
+      });
+  };
+
+  res.json(await searchImages("zelda"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
